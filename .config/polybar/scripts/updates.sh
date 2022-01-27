2f143d7305ac917e1cbc2dda111b16cc770aeb62
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-01-27 19:41:38 -0300

    Modified pacman/yay flags

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if ! updates_arch=$(pacman -Qum 2> /dev/null | wc -l ); then
+if ! updates_arch=$(pacman -Qu 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+if ! updates_aur=$(yay -Qu 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
 updates=$((updates_arch + updates_aur))
 
 if [ "$updates" -gt 0 ]; then
-    echo "# $updates"
+    echo "$updates"
 else
     echo "0"
 fi
