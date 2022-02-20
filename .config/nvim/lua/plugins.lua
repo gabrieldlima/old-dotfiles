41829f09504a4cd63cc0dede1594620bf4f82b68
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-02-20 14:38:07 -0300

    Plugin installed: lualine

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,5 +2,11 @@ return require("packer").startup(function()
 
   -- Packer: a plugin manager for Neovim
   use 'wbthomason/packer.nvim'
+  
+  -- Lualine: a statusline plugin written in pure lua
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 
 end)
