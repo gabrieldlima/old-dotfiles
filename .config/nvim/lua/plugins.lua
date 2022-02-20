b6ce75de94030973cb17e7d1226420bbd0c09954
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-02-20 15:20:55 -0300

    Plugin installed: nvim-tree

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -9,4 +9,11 @@ return require("packer").startup(function()
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
+  -- Nvim-Tree: a file explorer tree for neovim written in lua
+  use {
+      'kyazdani42/nvim-tree.lua',
+      requires = { 'kyazdani42/nvim-web-devicons' },
+      config = function() require'nvim-tree'.setup {} end
+  }
+
 end)
