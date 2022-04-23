dc7a4066c3ce8670ae82ce773654762dcfdf095d
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-04-23 18:23:34 -0300

    Enable borders to one single window

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -478,18 +478,6 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 --==============================================================================
 beautiful.useless_gap = 8
 
--- No borders when rearranging only 1 non-floating or maximized client
-screen.connect_signal("arrange", function (s)
-   local only_one = #s.tiled_clients == 1
-   for _, c in pairs(s.clients) do
-       if only_one and not c.floating or c.maximized then
-           c.border_width = 0
-       else
-           c.border_width = beautiful.border_width -- your border width
-       end
-   end
-end)
-
 
 --==============================================================================
 -- AUTOSTART APPLICATIONS
