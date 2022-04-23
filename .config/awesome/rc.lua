49da1b770be230060dc4625adf1338ebb4743d8f
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-04-23 18:30:31 -0300

    Disable hotkeys help widget for VIM and other apps

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -16,9 +16,6 @@ local beautiful = require("beautiful")
 -- Notification library
 local naughty = require("naughty")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
 
 
 --==============================================================================
