03fc628e518057ee40ac8dd52dfbc27ace112f1f
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-05-19 05:58:28 -0300

    Borders/Gaps

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -10,6 +10,11 @@ local theme = {}
 
 theme.font          = "Fira Code 8"
 
+-- Borders/Gaps
+theme.useless_gap   = dpi(7)
+theme.screen_margin = theme.useless_gap
+theme.border_width  = dpi(2)
+
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
 theme.bg_urgent     = "#ff0000"
@@ -21,8 +26,6 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
 theme.border_marked = "#91231c"
