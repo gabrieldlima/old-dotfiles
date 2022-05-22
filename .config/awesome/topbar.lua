67734202a3b0b058bbe5e41824383bcf55dc2373
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-05-22 20:38:17 -0300

    Update file

--- .config/awesome/topbar.lua
+++ .config/awesome/topbar.lua
@@ -9,46 +9,66 @@ require("widgets.keyboard")
 require("widgets.clock")
 
 
--- {{{ Wibar
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
-                    awful.button({ }, 1, function(t) t:view_only() end),
-                    awful.button({ modkey }, 1, function(t)
-                                              if client.focus then
-                                                  client.focus:move_to_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, function(t)
-                                              if client.focus then
-                                                  client.focus:toggle_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
-                )
+    awful.button(
+        { }, 1,
+        function(t) t:view_only() end
+    ),
+    awful.button(
+        { modkey }, 1, 
+        function(t) 
+            if client.focus then
+                client.focus:move_to_tag(t)
+            end
+        end
+    ),
+    awful.button(
+        { }, 3, 
+        awful.tag.viewtoggle
+    ),
+    awful.button(
+        { modkey }, 3,
+        function(t)
+            if client.focus then
+                client.focus:toggle_tag(t)
+            end
+        end
+    ),
+    awful.button(
+        { }, 4, 
+        function(t) awful.tag.viewnext(t.screen) end
+    ),
+    awful.button(
+        { }, 5,
+        function(t) awful.tag.viewprev(t.screen) end
+    )
+)
 
 local tasklist_buttons = gears.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  c:emit_signal(
-                                                      "request::activate",
-                                                      "tasklist",
-                                                      {raise = true}
-                                                  )
-                                              end
-                                          end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                          end))
+    awful.button(
+        { }, 1,
+        function (c)
+            if c == client.focus then
+                c.minimized = true
+            else
+                c:emit_signal("request::activate", "tasklist", {raise = true})
+            end
+        end
+    ),
+    awful.button(
+        { }, 3,
+        function() awful.menu.client_list({ theme = { width = 250 } }) end
+    ),
+    awful.button(
+        { }, 4, 
+        function () awful.client.focus.byidx(1) end
+    ),
+    awful.button(
+        { }, 5,
+        function () awful.client.focus.byidx(-1) end
+    )
+)
 
 awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
@@ -58,10 +78,23 @@ awful.screen.connect_for_each_screen(function(s)
     -- We need one layoutbox per screen.
     s.mylayoutbox = awful.widget.layoutbox(s)
     s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+        awful.button(
+            { }, 1, 
+            function () awful.layout.inc(1) end
+        ),
+        awful.button(
+            { }, 3, 
+            function () awful.layout.inc(-1) end
+        ),
+        awful.button(
+            { }, 4,
+            function () awful.layout.inc(1) end
+        ),
+        awful.button(
+            { }, 5,
+            function () awful.layout.inc(-1) end
+        )
+    ))
     -- Create a taglist widget
     s.mytaglist = awful.widget.taglist {
         screen  = s,
@@ -97,4 +130,3 @@ awful.screen.connect_for_each_screen(function(s)
         },
     }
 end)
--- }}}
