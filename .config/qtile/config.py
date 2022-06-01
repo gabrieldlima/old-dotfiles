c7d8f5dccf8dfe1bbc66c76fff344135f8aaccd3
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-06-01 20:48:49 -0300

    Setting Monadtall layout (Master+slave) as default

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -100,19 +100,18 @@ for i in groups:
     )
 
 layouts = [
+    layout.MonadTall(),
     layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Stack(num_stacks=2),
+    layout.Bsp(),
+    layout.Matrix(),
+    layout.MonadWide(),
+    layout.RatioTile(),
+    layout.Tile(),
+    layout.TreeTab(),
+    layout.VerticalTile(),
+    layout.Zoomy(),
 ]
 
 widget_defaults = dict(
