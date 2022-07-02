1bc980394892440023b107aad371741aedde3319
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-07-02 19:15:05 -0300

    Deleted polybar dir

--- .config/polybar/scripts/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-# Launch bar
-echo "---" | tee -a /tmp/polybar1.log 
-polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars launched..."
