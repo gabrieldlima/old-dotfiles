0111675a08805988621d68037aff76b0245eb6da
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-07-10 14:05:51 -0300

    New bar config and new modules

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,7 @@
 #!/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
-# Launch bar
-echo "---" | tee -a /tmp/polybar.log
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
-
-echo "Bars launched..."
+polybar -r distro-logo &
+polybar -r workspaces &
+polybar -r status &
