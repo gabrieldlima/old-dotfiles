9234cdd28b9c545b2f40f8de36f0b41823545276
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-07-10 14:08:02 -0300

    Added: xsetroot -cursor_name left_ptr; Removed some systray applets

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -2,8 +2,5 @@
 
 picom --experimental-backend &
 nitrogen --restore &
-nm-applet &
-volctl-applet &
-clipit-applet &
-blueman-applet &
+xsetroot -cursor_name left_ptr
 $HOME/.config/polybar/launch.sh &
