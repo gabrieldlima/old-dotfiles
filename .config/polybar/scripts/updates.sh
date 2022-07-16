eab14e6653f252e886c203071e483e114c3ba135
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-07-16 15:26:51 -0300

    Added updates script

---
+++ .config/polybar/scripts/updates.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+if ! updates_arch=$(pacman -Qum 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
+
+if [ "$updates" -gt 0 ]; then
+    echo "$updates"
+else
+    echo "$updates"
+fi
