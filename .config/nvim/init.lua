4a89d5d8896880b607957301d28a3af227f374fe
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-08-24 19:40:38 -0300

    Deleted neovim directory

--- .config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("options")
-require("plugins")
-require("colorscheme")
-
--- Plugins
-require('lualine').setup()
-require('nvim-tree').setup()
-require('nvim-treesitter')
-require('nvim-autopairs').setup()
