0619f91c893dbabc806e8c8ecc5f44ef0c454893
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-10-04 16:00:28 -0300

    Organized some settings

--- .config/polybar/scripts/redshift.sh
+++ .config/polybar/scripts/redshift.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 if [ $(pidof redshift) ]
 then
--- .config/polybar/scripts/toogle_redshift.sh
+++ .config/polybar/scripts/toogle_redshift.sh
@@ -1,10 +1,9 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 if [ $(pidof redshift) ]
 then
     redshift -x
     killall -q redshift
 else
-    dunstify "Night Light: 4500K"
     redshift
 fi
