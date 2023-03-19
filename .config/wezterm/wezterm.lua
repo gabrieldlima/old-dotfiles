e02100ab413683a65ab84860a3696225b896d398
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-03-19 14:54:45 -0300

    Disable check for udpates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,11 +4,12 @@ local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 custom.background = "#11111b"
 
 return {
+    check_for_updates = false,
+    font = wezterm.font 'Fira Code',
+    font_size = 10.0,
+
     color_schemes = {
         ["Catppuccin_Mocha"] = custom,
     },
     color_scheme = "Catppuccin_Mocha",
-
-    font = wezterm.font 'Fira Code',
-    font_size = 10.0
 }
