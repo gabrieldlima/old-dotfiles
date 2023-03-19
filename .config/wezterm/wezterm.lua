e6349bde75ec9859607c3cc2ae497c9109d7e5a7
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-03-19 13:32:31 -0300

    Catppuccin baby

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,1 +1,11 @@
-return {}
+local wezterm = require("wezterm")
+
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+custom.background = "#11111b"
+
+return {
+    color_schemes = {
+        ["Catppuccin_Mocha"] = custom,
+    },
+    color_scheme = "Catppuccin_Mocha",
+}
