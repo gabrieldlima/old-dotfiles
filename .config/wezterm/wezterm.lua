84be3589c49ea76ec4a793a2348a14cfc0617080
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-03-19 14:56:34 -0300

    Disable confirmation on window close

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ return {
     check_for_updates = false,
     enable_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,
+    window_close_confirmation = 'NeverPrompt',
     font = wezterm.font 'Fira Code',
     font_size = 10.0,
 
