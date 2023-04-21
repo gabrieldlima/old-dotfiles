b8efdec0cac40ce9ed03da4e131ce567b3e3320b
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-04-21 09:05:13 -0300

    Update the tasklist widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -159,14 +159,6 @@ screen.connect_signal("request::desktop_decoration", function(s)
     s.mytasklist = awful.widget.tasklist {
         screen  = s,
         filter  = awful.widget.tasklist.filter.focused,
-        buttons = {
-            awful.button({ }, 1, function (c)
-                c:activate { context = "tasklist", action = "toggle_minimization" }
-            end),
-            awful.button({ }, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
-            awful.button({ }, 4, function() awful.client.focus.byidx(-1) end),
-            awful.button({ }, 5, function() awful.client.focus.byidx( 1) end),
-        }
     }
 
     -- Create the wibox
