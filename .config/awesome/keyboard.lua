ad54c217f59b3fbea8cfc411b561e3dff39b9220
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-04-21 07:07:18 -0300

    Update file

--- .config/awesome/keyboard.lua
+++ .config/awesome/keyboard.lua
@@ -296,60 +296,84 @@ awful.keyboard.append_global_keybindings({
 
 client.connect_signal("request::default_mousebindings", function()
     awful.mouse.append_client_mousebindings({
-        awful.button({ }, leftclick, function (c)
-            c:activate { context = "mouse_click" }
-        end),
-        awful.button({ modkey }, leftclick, function (c)
-            c:activate { context = "mouse_click", action = "mouse_move"  }
-        end),
-        awful.button({ modkey }, rightclick, function (c)
-            c:activate { context = "mouse_click", action = "mouse_resize"}
-        end),
+        awful.button(
+            { }, leftclick,
+            function (c) c:activate { context = "mouse_click" } end
+        ),
+        awful.button(
+            { modkey }, leftclick,
+            function (c) c:activate { context = "mouse_click", action = "mouse_move" } end
+        ),
+        awful.button(
+            { modkey }, rightclick,
+            function (c) c:activate { context = "mouse_click", action = "mouse_resize" } end
+        ),
     })
 end)
 
 client.connect_signal("request::default_keybindings", function()
     awful.keyboard.append_client_keybindings({
-        awful.key({ modkey,           }, "f",
+        awful.key(
+            { modkey }, "f",
             function (c)
                 c.fullscreen = not c.fullscreen
                 c:raise()
             end,
-            {description = "toggle fullscreen", group = "client"}),
-        awful.key({ modkey, shiftkey   }, "c",      function (c) c:kill()                         end,
-            {description = "close", group = "client"}),
-        awful.key({ modkey, crtlkey }, "space",  awful.client.floating.toggle                     ,
-            {description = "toggle floating", group = "client"}),
-        awful.key({ modkey, crtlkey }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-            {description = "move to master", group = "client"}),
-        awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
-            {description = "move to screen", group = "client"}),
-        awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-            {description = "toggle keep on top", group = "client"}),
-        awful.key({ modkey,           }, "n",
-            function (c)
-                -- The client currently has the input focus, so it cannot be
-                -- minimized, since minimized clients can't have the focus.
-                c.minimized = true
-            end ,
-            {description = "minimize", group = "client"}),
-        awful.key({ modkey,           }, "m",
+            { description = "toggle fullscreen", group = "client" }
+        ),
+        awful.key(
+            { modkey, shiftkey }, "c",
+            function (c) c:kill() end,
+            { description = "close", group = "client" }
+        ),
+        awful.key(
+            { modkey, crtlkey }, "space",
+            awful.client.floating.toggle,
+            { description = "toggle floating", group = "client" }
+        ),
+        awful.key(
+            { modkey, crtlkey }, "Return",
+            function (c) c:swap(awful.client.getmaster()) end,
+            { description = "move to master", group = "client" }
+        ),
+        awful.key(
+            { modkey }, "o",
+            function (c) c:move_to_screen() end,
+            { description = "move to screen", group = "client" }
+        ),
+        awful.key(
+            { modkey }, "t",
+            function (c) c.ontop = not c.ontop end,
+            { description = "toggle keep on top", group = "client" }
+        ),
+        awful.key(
+            { modkey }, "n",
+            function (c) c.minimized = true end ,
+            { description = "minimize", group = "client" }
+        ),
+        awful.key(
+            { modkey }, "m",
             function (c)
                 c.maximized = not c.maximized
                 c:raise()
             end ,
-            {description = "(un)maximize", group = "client"}),
-        awful.key({ modkey, crtlkey }, "m",
+            { description = "(un)maximize", group = "client" }
+        ),
+        awful.key(
+            { modkey, crtlkey }, "m",
             function (c)
                 c.maximized_vertical = not c.maximized_vertical
                 c:raise()
             end ,
-            {description = "(un)maximize vertically", group = "client"}),
-        awful.key({ modkey, shiftkey   }, "m",
+            { description = "(un)maximize vertically", group = "client" }
+        ),
+        awful.key(
+            { modkey, shiftkey }, "m",
             function (c)
                 c.maximized_horizontal = not c.maximized_horizontal
                 c:raise()
             end ,
-            {description = "(un)maximize horizontally", group = "client"}),
+            { description = "(un)maximize horizontally", group = "client" }
+        ),
     })
 end)
