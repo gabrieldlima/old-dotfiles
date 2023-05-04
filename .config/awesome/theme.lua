a7c4a25864a5523551ab8bf991a19f05afebf7a9
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-05-04 20:43:20 -0300

    Removed square from tag list

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -383,13 +383,6 @@ theme.taglist_spacing                     = 5
 -- theme.taglist_shape_border_width_volatile = nil
 -- theme.taglist_shape_border_color_volatile = nil
 -- Generate taglist squares:
-local taglist_square_size = dpi(4)
-theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
-    taglist_square_size, theme.fg_normal
-)
-theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
-    taglist_square_size, theme.fg_normal
-)
 
 -- tasklist
 -- theme.tasklist_fg_normal                    = nil
