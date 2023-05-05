ea86669ff1d026912ddef71f6f0897beeec4eb32
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-05-04 21:09:02 -0300

    Update aliases for my fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,40 @@
 if status is-interactive
-    # Aliases
+    # Changing "ls" to "exa"
     alias ls='exa --long --group-directories-first --icons'
     alias ll='exa --long --group-directories-first --icons'
     alias la='exa --long --all --group-directories-first --icons'
     alias lt='exa --long --tree --group-directories-first --icons'
-    alias cp='cp -v'
-    alias mv='mv -v'
-    alias rm='rm -v'
-    alias mkdir='mkdir -p'
-    alias rmdir='rmdir -p'
+
+    # Modern Unix tools
+    alias cat='bat'
+    alias df="duf"
+
+    # Confirm before overwriting something
+    alias cp='cp --verbose --interactive'
+    alias mv='mv --verbose --interactive'
+    alias rm='rm --verbose --interactive'
+
+    # Adding flags
+    alias mkdir='mkdir --parents'
+    alias rmdir='rmdir --parents'
+
+    # Colorize grep output (good for log files)
     alias grep='grep --color=auto'
     alias diff='diff --color=auto'
     alias tree='tree -C'
     alias ip='ip -color'
-    alias cat='bat'
+
+    # Neovim (the best text editor ever)
+    alias v='nvim'
     alias vi='nvim'
     alias vim='nvim'
-    alias df="duf"
-    alias lf='lfub'
+    alias nano='nvim'
+    alias emacs='nvim'
+
+    # For dotfiles
     alias dot='/usr/bin/git --git-dir=$HOME/.local/share/dotfiles/ --work-tree=$HOME'
     alias token='cat $HOME/.config/token | xclip -selection clipboard'
-end
 
-starship init fish | source
+    # Blazing-fast 🦅starship prompt
+    starship init fish | source
+end
