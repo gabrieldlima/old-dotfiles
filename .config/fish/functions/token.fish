f5c49440e12ed0b6b868ec98841b2ace88138a66
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-07-01 20:42:01 -0300

    changed from xclip to wl-clipboard

--- .config/fish/functions/token.fish
+++ .config/fish/functions/token.fish
@@ -1,3 +1,3 @@
-function token --wraps='cat $HOME/.config/token | xclip -selection clipboard' --description 'alias token=cat $HOME/.config/token | xclip -selection clipboard'
-    cat $HOME/.config/token | xclip -selection clipboard $argv
+function token --wraps='cat $HOME/.config/token | wl-copy' --description 'alias token=cat $HOME/.config/token | wl-copy'
+    cat $HOME/.config/token | wl-copy $argv
 end
