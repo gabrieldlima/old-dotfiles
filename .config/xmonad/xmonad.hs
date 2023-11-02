41ff4031a40b3e3b666d7230b4f06475cb5e52c6
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-02 00:29:02 -0300

    My initial layout definition

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,16 +1,35 @@
+-- ╔══════════════════════════════════════════════════════╗
+-- ║██╗  ██╗███╗   ███╗ ██████╗ ███╗   ██╗ █████╗ ██████╗ ║
+-- ║╚██╗██╔╝████╗ ████║██╔═══██╗████╗  ██║██╔══██╗██╔══██╗║
+-- ║ ╚███╔╝ ██╔████╔██║██║   ██║██╔██╗ ██║███████║██║  ██║║
+-- ║ ██╔██╗ ██║╚██╔╝██║██║   ██║██║╚██╗██║██╔══██║██║  ██║║
+-- ║██╔╝ ██╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║██║  ██║██████╔╝║
+-- ║╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ║
+-- ╚══════════════════════════════════════════════════════╝
+
 import XMonad
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Magnifier
 
 main :: IO ()
 main = xmonad $ def
     {
-      modMask = mod4Mask -- Rebind Mod to the Super key
+      modMask = mod4Mask    -- Rebind Mod to the Super key
+    , layoutHook = myLayout -- Use custom layouts
     }
   `additionalKeysP`
     [
-      ("M-b", spawn "qutebrowser") -- Browser
+      ("M-b", spawn "qutebrowser")     -- Browser
     , ("M-p", spawn "rofi -show drun") -- Launcher
-    , ("M-<Return>", spawn "wezterm") -- Terminal
+    , ("M-<Return>", spawn "wezterm")  -- Terminal
     ]
+myLayout = tiled ||| Mirror tiled ||| threeCol ||| Full
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled = Tall nmaster delta ratio
+    nmaster = 1     -- Default number of windows in the master pane
+    ratio = 1/2     -- Default proportion of screen occupied by master pane
+    delta = 3/100   -- Percent of screen to increment by when resizing panes
