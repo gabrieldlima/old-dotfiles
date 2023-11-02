15dbc3052efc2c57da43032e1b366dab8ddd0d70
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-01 23:33:08 -0300

    Add some binds for default apps

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -5,5 +5,12 @@ import XMonad.Util.Ungrab
 
 main :: IO ()
 main = xmonad $ def
-    { modMask = mod4Mask -- Rebind Mod to the Super key
+    {
+      modMask = mod4Mask -- Rebind Mod to the Super key
     }
+  `additionalKeysP`
+    [
+      ("M-b", spawn "qutebrowser") -- Browser
+    , ("M-p", spawn "rofi -show drun") -- Launcher
+    , ("M-<Return>", spawn "wezterm") -- Terminal
+    ]
